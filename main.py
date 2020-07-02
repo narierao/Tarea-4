93b4b6464ea13011e67884f324462cc107f6353c
narierao <nariera@uc.cl>
2020-07-02 00:22:25 -0400

    Ejercicio 1.a completo

--- main.py
+++ main.py
@@ -63,13 +63,22 @@ def DaySimulation():
 
     # time in simulation, max and actual
     max_time = 12*60*60
-    actual_time = 2*60*60
+    actual_time = 0
 
     # data for analysis 
     attended_client = []
     lost_clients = 0
     empty = 0
-
+    empty_last_update = 0
+    full_cashers = 0
+    full_cashers_last_update = 0
+    long_queue_time = {}
+    long_queue_recurrence = {}
+    for i in range(0,28):
+        long_queue_time[i] = 0
+        long_queue_recurrence[i] = 0
+    last_update_queue = 0
+    
     # client next product processed by cashier
     client1_next_product = max_time +1 
     client2_next_product = max_time +1
@@ -95,7 +104,7 @@ def DaySimulation():
 
         client_looking_for = sorted(client_looking_for, key = lambda client: client.looking_for)
         next_event = get_next_event(client1_next_product, client2_next_product, client3_next_product, next_client_at, client_looking_for )
-
+        
         # arrive a client
         if next_event == next_client_at:
             n = 0
@@ -143,6 +152,12 @@ def DaySimulation():
                     client1_in_casher.queue_finish = actual_time
 
                 if queue_client_list != []:
+
+                    # update queue data
+                    long_queue_time[len(queue_client_list)] += actual_time - last_update_queue
+                    last_update_queue = actual_time
+                    long_queue_recurrence[len(queue_client_list)] += 1
+                    
                     client1_in_casher = queue_client_list.pop(0)
                     
                     if actual_time >= 4*60*60 and actual_time <= 10*60*60:
@@ -178,6 +193,12 @@ def DaySimulation():
                     client2_in_casher.queue_finish = actual_time
 
                 if queue_client_list != []:
+
+                    # update queue data
+                    long_queue_time[len(queue_client_list)] += actual_time - last_update_queue
+                    last_update_queue = actual_time
+                    long_queue_recurrence[len(queue_client_list)] += 1
+                    
                     client2_in_casher = queue_client_list.pop(0)
 
                     if actual_time >= 4*60*60 and actual_time <= 10*60*60:
@@ -211,6 +232,11 @@ def DaySimulation():
                     client3_in_casher.queue_finish = actual_time
                     
                 if queue_client_list != []:
+                    
+                    # update queue data
+                    long_queue_time[len(queue_client_list)] += actual_time - last_update_queue
+                    last_update_queue = actual_time
+                    long_queue_recurrence[len(queue_client_list)] += 1
                     
                     client3_in_casher = queue_client_list.pop(0)
 
@@ -281,28 +307,77 @@ def DaySimulation():
                         cashier3.change_params(4, 16, 0)
                     client3_next_product = cashier3.generate_next_product_attendance(actual_time) 
             else:
+
+                # update queue data
+                long_queue_time[len(queue_client_list)] += actual_time - last_update_queue
+                last_update_queue = actual_time
+                long_queue_recurrence[len(queue_client_list)] += 1
+                
                 client = client_looking_for.pop(0)
                 client.queue_start = actual_time
                 queue_client_list.append(client)
-                
-        #print("Largo de la cola: ", len(queue_client_list))
-        #print("Gente buscando en la tienda: ", len(client_looking_for))
-        #print("Clientes en el sistema: ", len(queue_client_list)+ len(client_looking_for))
-        # end simultation if 
+
+        if client1_in_casher == None and client2_in_casher == None and client3_in_casher == None:
+            empty += actual_time - empty_last_update
+            empty_last_update = actual_time
+        else:
+            empty_last_update = actual_time
+            
+        if client1_in_casher != None and client2_in_casher != None and client3_in_casher != None:
+            full_cashers += actual_time - full_cashers_last_update
+            full_cashers_last_update = actual_time
+        else:
+            full_cashers_last_update = actual_time
+        
         if next_event >= 12*60*60:
             #print("Fin de la simulación")
             n = 0
             queue_time = 0
+
+            # queue mean for a client
             for i in attended_client:
                 if i.queue_finish - i.queue_start != 0:
                     queue_time += i.queue_finish - i.queue_start
                     n += 1
             queue_mean = queue_time/n
-            return (len(attended_client), queue_mean, ) 
-        
-        
 
-print(DaySimulation())
+            # mean long queue
+            mean_queue_long = 0
+            s = 0
+            for key in list(long_queue_time.keys()):
+                s += long_queue_time[key]            
+            for key in list(long_queue_time.keys()):
+                mean_queue_long += key * long_queue_time[key] / s
+
+            #print(empty)
+            #print(full_cashers/(12*60*60))
+            
+            return (len(attended_client), queue_mean, mean_queue_long, empty, full_cashers/(12*60*60), lost_clients) 
+        
+def simlations():
+    attended_clients = []
+    queue_means = []
+    mean_queue_longs = []
+    emptys = []
+    fully_cashers = []
+    lost_clients = []
+    for i in range(100):
+        a,b,c,d,e,f = DaySimulation()
+        attended_clients.append(a)
+        queue_means.append(b)
+        mean_queue_longs.append(c)
+        emptys.append(d)
+        fully_cashers.append(e)
+        lost_clients.append(f)
+    print(np.mean(attended_clients))
+    print(np.mean(queue_means))
+    print(np.mean(mean_queue_longs))
+    print(np.mean(emptys))
+    print(np.mean(fully_cashers))
+    print(np.mean(lost_clients))
+
+
+print(simlations())
 
 
 
